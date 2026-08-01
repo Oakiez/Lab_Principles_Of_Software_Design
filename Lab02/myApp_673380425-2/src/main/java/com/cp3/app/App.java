2191f834a4429bf780dd18786b39307743f9214f
Oakiez <wongsathon.th@kkumail.com>
2026-08-01 18:29:31 +0700

    Added: Lab2 and Lab4

---
+++ Lab02/myApp_673380425-2/src/main/java/com/cp3/app/App.java
@@ -0,0 +1,10 @@
+package com.cp3.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Wongsathon Thanayod, Student ID: 673380425-2 sec:3");
+    }
+}
