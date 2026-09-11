ee31785e890dea642b6e5e3d807e5821865966e4
Oakiez <wongsathon.th@kkumail.com>
2026-09-11 23:44:07 +0700

    Added: Lab8

---
+++ Lab8/demo/src/main/java/com/example/demo/strategy/NoDiscountStrategy.java
@@ -0,0 +1,8 @@
+package com.example.demo.strategy;
+
+public class NoDiscountStrategy implements DiscountStrategy {
+    @Override
+    public double apply(double price) {
+        return price;
+    }
+}
